ed5ea850b9f8cf1a9083077bf19f89beab86cd5f
Bartekus <Bartekus@gmail.com>
2016-02-16 00:12:53 -0700

    removed unused code

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,5 @@
 import {render} from 'react-dom';
 import React from 'react';
-import {syncHistory, routeReducer} from 'react-router-redux'
 import {Map, fromJS} from 'immutable';
 import {ensureState} from 'redux-optimistic-ui';
 
