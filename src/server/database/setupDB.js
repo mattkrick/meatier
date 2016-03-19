92017530d29cc9de4756ae19271e153808e29053
Alec Wenzowski <alec@wenzowski.com>
2016-03-19 17:55:41 -0400

    resolve promises in parallel

--- src/server/database/setupDB.js
+++ src/server/database/setupDB.js
@@ -10,17 +10,25 @@ const database = [
 ];
 
 export default async function setupDB() {
-  for (let db of databases) {
-    try {
-      await r.dbCreate(db);
-    } catch (e) {
-    }
-    let indices = [];
-    const tables = await r.db(db).tableList().run();
-    await Promise.all(tables.map(table => r.db(db).tableDrop(table)));
-    await Promise.all(database.map(table => r.db(db).tableCreate(table.name)));
-    database.forEach(table => table.indices.forEach(index => indices.push(r.db(db).table(table.name).indexCreate(index))));
-    await Promise.all(indices);
-  }
+  await Promise.all(databases.map(reset));
   await r.getPool().drain();
+}
+
+async function reset(db) {
+  try {
+    console.log('creating', db);
+    await r.dbCreate(db);
+  } catch (e) {
+    console.log(e && e.message);
+  }
+  const indices = [];
+  const tables = await r.db(db).tableList().run();
+  console.log('dropping all tables on', db);
+  await Promise.all(tables.map(table => r.db(db).tableDrop(table)));
+  console.log('creating new tables on', db);
+  await Promise.all(database.map(table => r.db(db).tableCreate(table.name)));
+  console.log('adding indexes to tables on', db);
+  database.forEach(table => table.indices.forEach(index => indices.push(r.db(db).table(table.name).indexCreate(index))));
+  await Promise.all(indices);
+  console.log('set up', db);
 }
