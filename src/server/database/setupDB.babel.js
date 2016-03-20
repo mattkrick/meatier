cea2bb5774bab6c06d8941f0112089c46cec0f74
mattkrick <matt.krick@gmail.com>
2016-03-20 13:52:58 -0600

    allow for db update without wiping it clean.

--- src/server/database/setupDB.babel.js
+++ src/server/database/setupDB.babel.js
@@ -1,4 +1,4 @@
 require('babel-register');
 require('babel-polyfill');
-require('./setupDB.js')();
+require('./setupDB.js')(process.argv[2]);
 
--- src/server/database/setupDB.js
+++ src/server/database/setupDB.js
@@ -9,26 +9,40 @@ const database = [
   {name: 'notes', indices: ['laneId']}
 ];
 
-export default async function setupDB() {
-  await Promise.all(databases.map(reset));
+export default async function setupDB(isUpdate = false) {
+  await Promise.all(databases.map(db => ({db, isUpdate})).map(reset));
   await r.getPool().drain();
+  console.log(`>>Database setup complete!`);
 }
 
-async function reset(db) {
-  try {
-    console.log('creating', db);
+async function reset({db, isUpdate}) {
+  const dbList = await r.dbList();
+  if (dbList.indexOf(db) === -1) {
+    console.log(`>>Creating Database: ${db}`);
     await r.dbCreate(db);
-  } catch (e) {
-    console.log(e && e.message);
   }
+  let tables = await r.db(db).tableList();
+  if (!isUpdate) {
+    console.log(`>>Dropping tables on: ${db}`);
+    await Promise.all(tables.map(table => r.db(db).tableDrop(table)));
+  }
+  tables = await r.db(db).tableList();
+  console.log(`>>Creating tables on: ${db}`);
+  await Promise.all(database.map(table => {
+    if (tables.indexOf(table.name) === -1) {
+      return r.db(db).tableCreate(table.name)
+    }
+  }));
+  console.log(`>>Adding table indices on: ${db}`);
   const indices = [];
-  const tables = await r.db(db).tableList().run();
-  console.log('dropping all tables on', db);
-  await Promise.all(tables.map(table => r.db(db).tableDrop(table)));
-  console.log('creating new tables on', db);
-  await Promise.all(database.map(table => r.db(db).tableCreate(table.name)));
-  console.log('adding indexes to tables on', db);
-  database.forEach(table => table.indices.forEach(index => indices.push(r.db(db).table(table.name).indexCreate(index))));
+  database.forEach(async (table) => {
+    const indexList = await r.db(db).table(table).indexList();
+    table.indices.forEach(index => {
+      if (indexList.indexOf(index) === -1) {
+        indices.push(r.db(db).table(table.name).indexCreate(index))
+      }
+    })
+  });
   await Promise.all(indices);
-  console.log('set up', db);
+  console.log(`>>Setup complete for: ${db}`);
 }
