6960d01c07a10066ee107ab09c7ee9c989b7de19
mattkrick <matt.krick@gmail.com>
2016-03-24 16:57:11 -0600

    add corrected getFields

--- src/server/graphql/models/utils.js
+++ src/server/graphql/models/utils.js
@@ -38,34 +38,34 @@ export const makeRequired = (fields, requiredFieldNames) => {
   return newFields;
 };
 
-// export function getFields(context, asts = context.fieldASTs) {
-//  // for recursion...Fragments doesn't have many sets...
-//  if (!Array.isArray(asts)) asts = [asts];
-//
-//  // get all selectionSets
-//  var selections = asts.reduce((selections, source) => {
-//    selections.push(...source.selectionSet.selections);
-//    return selections;
-//  }, []);
-//
-//  // return fields
-//  return selections.reduce((list, ast) => {
-//    switch (ast.kind) {
-//      case 'Field' :
-//        list[ast.name.value] = true;
-//        return list;
-//      case 'InlineFragment':
-//        return {
-//          ...list,
-//          ...getFieldList(context, ast)
-//        };
-//      case 'FragmentSpread':
-//        return {
-//          ...list,
-//          ...getFieldList(context, context.fragments[ast.name.value])
-//        };
-//      default:
-//        throw new Error('Unsuported query selection');
-//    }
-//  }, {});
-// }
+ export function getFields(context, asts = context.fieldASTs) {
+  // for recursion...Fragments doesn't have many sets...
+  if (!Array.isArray(asts)) asts = [asts];
+
+  // get all selectionSets
+  var selections = asts.reduce((selections, source) => {
+    selections.push(...source.selectionSet.selections);
+    return selections;
+  }, []);
+
+  // return fields
+  return selections.reduce((list, ast) => {
+    switch (ast.kind) {
+      case 'Field' :
+        list[ast.name.value] = true;
+        return list;
+      case 'InlineFragment':
+        return {
+          ...list,
+          ...getFields(context, ast)
+        };
+      case 'FragmentSpread':
+        return {
+          ...list,
+          ...getFields(context, context.fragments[ast.name.value])
+        };
+      default:
+        throw new Error('Unsuported query selection');
+    }
+  }, {});
+ }
