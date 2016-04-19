968ff4e5a2f5aef3a4763acce310407d057ca209
James Cavanaugh <cavanaughjames@gmail.com>
2016-04-19 09:49:40 -0400

    Update noteSchema.js

--- src/server/graphql/models/Notes/noteSchema.js
+++ src/server/graphql/models/Notes/noteSchema.js
@@ -16,17 +16,17 @@ export const Note = new GraphQLObjectType({
     id: {type: new GraphQLNonNull(GraphQLID), description: 'The noteId'},
     userId: {type: new GraphQLNonNull(GraphQLID), description: 'The userId that created the lane'},
     laneId: {type: new GraphQLNonNull(GraphQLID), description: 'The laneId that the note belongs to'},
-    title: {type: new GraphQLNonNull(GraphQLTitleType), description: 'The lane title'},
+    title: {type: new GraphQLNonNull(GraphQLTitleType), description: 'The note title'},
     index: {type: new GraphQLNonNull(GraphQLInt), description: 'The index of the note in its lane'},
-    createdAt: {type: GraphQLString, description: 'The datetime the lane was created'},
-    updatedAt: {type: GraphQLString, description: 'The datetime the lane was last updated'}
+    createdAt: {type: GraphQLString, description: 'The datetime the note was created'},
+    updatedAt: {type: GraphQLString, description: 'The datetime the note was last updated'}
   })
 });
 
 const inputFields = {
   id: {type: GraphQLID, description: 'The laneId'},
   userId: {type: GraphQLID, description: 'The userId that created the lane'},
-  title: {type: GraphQLTitleType, description: 'The lane title'},
+  title: {type: GraphQLTitleType, description: 'The note title'},
   index: {type: GraphQLInt, description: 'The index of the note in its lane'},
   laneId: {type: GraphQLID, description: 'The laneId that the note belongs to'}
 };
