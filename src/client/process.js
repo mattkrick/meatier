ca170bbe958cd04fd488f52d7dc8cf2c7b2bb381
Andy Edwards <jedwards@fastmail.com>
2016-07-15 21:22:23 -0500

    fix eslint error

--- src/client/process.js
+++ src/client/process.js
@@ -1,5 +1,7 @@
 import libs from 'node-libs-browser';
 // libs exports a map of module numbers, not the modules themselves
+/* eslint-disable no-undef */
 const process = __webpack_require__(libs.process);
+/* eslint-enable no-undef */
 // overwrite env with variables that have been provided from a SSR script
-export default {...process, env: window.__env__}
+export default {...process, env: window.__env__};
